6da96a07b7e68a491d7c6bd86ba5ba60b1e80afc
tanushreeBarsainyan <tanubarsainyan@gmail.com>
2023-11-23 20:07:57 -0800

    initial commit

---
+++ src/test/java/com/example/TodTalk/TodTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TodTalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
